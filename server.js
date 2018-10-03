3b8a006ad6d39eb89481b99be0408e55a960becb
RooiGevaar19 <redthreat19@gmail.com>
2018-10-03 13:58:00 +0200

    Hello World in Jade

---
+++ server.js
@@ -0,0 +1,25 @@
+//jshint esversion:6, node:true
+let express = require('express');  
+let app = express();
+
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname+'/public'));
+app.use('/scripts', express.static(__dirname + '/node_modules'));
+
+
+// ========= SERVER RUNNING
+
+app.get("/", function(req, res) {
+    res.render("layout", {});
+});
+
+let port = process.env.port | 8080;
+
+http.listen(port);
+
+console.log("Magic happens on port " + port);
